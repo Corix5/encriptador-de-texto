b34f05a33c3bbe3f74c7ea3d44c69ff5eedc5dc3
David Cortés <luisdavid202075@gmail.com>
2024-07-03 08:41:38 -0600

    Nueva version con modo oscuro y seccion acerca de mi

--- public/js/efectos.js
+++ public/js/efectos.js
@@ -1,19 +1,12 @@
-function abrirNav(){
-    document.querySelector('nav').classList.add('open');
-    //agregamos opacity 1 al nav-list para que sea visible
-    document.getElementById('nav-list').style.opacity = '1';
-    //agregamos style display none al icono de abrir
-    document.getElementById('open-nav-icon').style.display = 'none';
-    //agregamos style display block al icono de cerrar
-    document.getElementById('close-nav-icon').style.display = 'block';
-}
-
-function cerrarNav(){
-    document.querySelector('nav').classList.remove('open');
-    //agregamos opacity 0 al nav-list para que sea invisible
-    document.getElementById('nav-list').style.opacity = '0';
-    //agregamos style display block al icono de abrir
-    document.getElementById('open-nav-icon').style.display = 'block';
-    //agregamos style display none al icono de cerrar
-    document.getElementById('close-nav-icon').style.display = 'none';
+function toggleNav() {
+    const nav = document.querySelector('nav');
+    const openNavIcon = document.getElementById('open-nav-icon');
+    
+    if (nav.classList.contains('open')) {
+        nav.classList.remove('open');
+        openNavIcon.classList.remove('is-active');
+    } else {
+        nav.classList.add('open');
+        openNavIcon.classList.add('is-active');
+    }
 }
